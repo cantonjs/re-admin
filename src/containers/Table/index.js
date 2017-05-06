97c27bc1580a786945fa5a83cf6181f4bbe12cb1
3went2 <webb.png@gmail.com>
2017-05-06 16:36:40 +0800

    add Table container

---
+++ src/containers/Table/index.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Table extends Component {
+	render() {
+		return (
+			<h1>Table</h1>
+		);
+	}
+}
+
