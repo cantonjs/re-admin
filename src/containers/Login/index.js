834af2256aae359962ffde18d3c9c0862a1fd356
3went2 <webb.png@gmail.com>
2017-05-09 16:08:15 +0800

    add Login page

---
+++ src/containers/Login/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export default function Login() {
+	return (
+		<p>Login</p>
+	);
+}
--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from 'containers/App';
 import Home from 'containers/Home';
+import Login from 'containers/Login';
 import NotFound from 'containers/NotFound';
 import getSidebar from 'utils/getSidebar';
 
@@ -23,6 +24,7 @@ export default class Root extends Component {
 	render() {
 		return (
 			<Router history={browserHistory}>
+				<Route path="login" component={Login} />
 				<Route path="/" component={App}>
 					<IndexRoute component={Home}/>
 					{mapRoutes(getSidebar())}
