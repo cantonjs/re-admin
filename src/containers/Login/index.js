4b2ec0739d2f5c7ecf4fa57cde825d2580501295
JC <jc@ckoo.me>
2017-05-09 16:42:59 +0800

    add login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,8 +1,61 @@
 
-import React from 'react';
+import $$ from './style.scss';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { name } from 'config/app.js';
 
-export default function Login() {
-	return (
-		<p>Login</p>
-	);
+const FormItem = Form.Item;
+
+@Form.create()
+export default class Login extends Component {
+	static propTypes = {
+		form: PropTypes.object.isRequired,
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+		});
+	}
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div className={$$.container}>
+				<h1 className={$$.title}>{ name }</h1>
+				<Form onSubmit={this.handleSubmit} className={$$.form}>
+					<FormItem>
+						{getFieldDecorator('userName', {
+							rules: [{ required: true, message: 'Please input your username!' }],
+						})(
+							<Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+						)}
+					</FormItem>
+					<FormItem>
+						{getFieldDecorator('password', {
+							rules: [{ required: true, message: 'Please input your Password!' }],
+						})(
+							<Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+						)}
+					</FormItem>
+					<FormItem>
+						{getFieldDecorator('remember', {
+							valuePropName: 'checked',
+							initialValue: true,
+						})(
+							<Checkbox>Remember me</Checkbox>
+						)}
+						<Button type="primary" htmlType="submit" className={$$.bt}>
+							Log in
+						</Button>
+					</FormItem>
+				</Form>
+			</div>
+		);
+	}
 }
+
+
