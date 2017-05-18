3cd7d77bd0025b8f2b0acddc8d44698865f34c3d
3went2 <webb.png@gmail.com>
2017-05-18 18:25:29 +0800

    update build script

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -37,7 +37,7 @@ export default (env = {}) => {
 					include: [srcDir, testDir],
 					loader: 'babel-loader',
 					options: {
-						forceEnv: build ? 'build' : 'dev',
+						forceEnv: build ? 'es' : 'demo',
 					},
 				},
 				{
