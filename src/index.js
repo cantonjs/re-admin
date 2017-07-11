4300f962ad581ef6fd520b2517bbcc0262720392
3went2 <webb.png@gmail.com>
2017-07-11 11:54:56 +0800

    expose `DataStore` api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * from './schemas';
 export * from './fields';
 export * from './actions';
 export authStore from 'stores/authStore';
+export DataStore from 'stores/DataStore';
 export withField from 'utils/withField';
 export withActions from 'utils/withActions';
 export Admin from 'containers/Admin';
