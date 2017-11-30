3670ed91fa1f9299ec75f6f4423b678cc8d31366
3went2 <webb.png@gmail.com>
2017-11-30 19:08:37 +0800

    expose joinKeys method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 export * from './schemas';
 export * from './fields';
+
 export routerStore from 'stores/routerStore';
 export authStore from 'stores/authStore';
 export DataStore from 'stores/DataStore';
-export withField from 'utils/withField';
-export withActions from 'utils/withActions';
+
 export Admin from 'containers/Admin';
 export Toolbar from 'components/Toolbar';
 export CreateButton from 'components/CreateButton';
@@ -13,3 +13,8 @@ export UpdateButton from 'components/UpdateButton';
 export RemoveButton from 'components/RemoveButton';
 export RefButton from 'components/RefButton';
 export TableQuerySwitch from 'components/TableQuerySwitch';
+
+export withField from 'utils/withField';
+export withActions from 'utils/withActions';
+
+export joinKeys from 'utils/joinKeys';
