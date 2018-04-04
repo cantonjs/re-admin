4108b0c10f9995423bdce2cf7512c35fa6f35daa
3went2 <webb.png@gmail.com>
2018-04-04 19:39:29 +0800

    update i18n

--- src/stores/localeStore.js
+++ src/stores/localeStore.js
@@ -1,5 +1,5 @@
 import { observable, action } from 'mobx';
-import defaultLocale from 'locales/en.json';
+import defaultLocale from 'locales/en_US.json';
 
 const localeStore = observable({
 	...defaultLocale,
