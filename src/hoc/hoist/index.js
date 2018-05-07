aa7eccf770c09c152130b2e5e3342c90418ed0a3
3went2 <webb.png@gmail.com>
2018-05-07 18:14:08 +0800

    fix hoc/hoist bug

--- src/hoc/hoist/index.js
+++ src/hoc/hoist/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { forwardRef } from 'create-react-ref';
 
+const isStatelessComponent = function isStatelessComponent(component) {
+	return !!component.prototype.render;
+};
+
 export default function hoist(WrappedComponent, options = {}) {
 	const { displayName } = options;
 	return function createHoistedComponent(TargetComponent) {
 		let HoistedComponent = TargetComponent;
-
-		if (HoistedComponent.prototype.render) {
+		if (
+			!isStatelessComponent(TargetComponent) &&
+			!isStatelessComponent(WrappedComponent)
+		) {
 			HoistedComponent = forwardRef((props, ref) => (
 				<TargetComponent {...props} forwardedRef={ref} />
 			));
+			HoistedComponent.defaultProps = TargetComponent.defaultProps;
 		}
 
 		if (WrappedComponent) {
