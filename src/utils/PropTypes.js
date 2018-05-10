a3578469631078d808e676ebf21a14c4053fc763
3went2 <webb.png@gmail.com>
2018-05-10 18:35:15 +0800

    update PropTypes.component

--- src/utils/PropTypes.js
+++ src/utils/PropTypes.js
@@ -1,4 +1,3 @@
-
 import PropTypes from 'prop-types';
 import { propTypes as MobxPropTypes } from 'mobx-react';
 
@@ -7,37 +6,19 @@ const warned = {};
 export default {
 	...PropTypes,
 	component: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.func,
-	]),
-	array: PropTypes.oneOfType([
-		PropTypes.array,
-		MobxPropTypes.observableArray,
-	]),
-	stringOrNumber: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.number,
-	]),
-	stringOrObject: PropTypes.oneOfType([
-		PropTypes.string,
 		PropTypes.object,
-	]),
-	stringOrFunc: PropTypes.oneOfType([
 		PropTypes.string,
 		PropTypes.func,
 	]),
-	nodeOrFunc: PropTypes.oneOfType([
-		PropTypes.node,
-		PropTypes.func,
-	]),
-	render: PropTypes.oneOfType([
-		PropTypes.bool,
-		PropTypes.func,
-	]),
+	array: PropTypes.oneOfType([PropTypes.array, MobxPropTypes.observableArray]),
+	stringOrNumber: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	stringOrObject: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+	stringOrFunc: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+	nodeOrFunc: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+	render: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
 	deprecated: function deprecatedProp(propType, explanation) {
 		return function validate(props, propName, componentName, ...rest) {
 			if (props[propName] != null) {
-
 				// eslint-disable-next-line max-len
 				const message = `"${propName}" property of "${componentName}" has been deprecated. ${explanation}`;
 
@@ -48,5 +29,5 @@ export default {
 
 			return propType(props, propName, componentName, ...rest);
 		};
-	}
+	},
 };
