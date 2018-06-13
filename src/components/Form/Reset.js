2c318c428b9e322016739817f2d599182a70de4e
3went2 <webb.png@gmail.com>
2018-06-13 17:35:19 +0800

    fix Reset button bug

--- src/components/Form/Reset.js
+++ src/components/Form/Reset.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createRef } from 'utils/reactPolyfill';
 import PropTypes from 'prop-types';
 import { Form, Button } from 'antd';
 import { DemonButton } from 'react-form-mobx';
@@ -10,13 +11,22 @@ export default class Reset extends Component {
 		wrapperStyle: PropTypes.object,
 	};
 
+	demonButtonRef = createRef();
+
+	handleClick = (ev) => {
+		ev.preventDefault();
+		const demon = this.demonButtonRef.current;
+		demon.clear();
+		demon.submit();
+	};
+
 	render() {
 		const { wrapperStyle, ...props } = this.props;
 		return (
-			<DemonButton forwardedProps={props} type="reset">
+			<DemonButton forwardedProps={props} ref={this.demonButtonRef}>
 				{(forwardedProps) => (
 					<Item style={wrapperStyle}>
-						<Button {...forwardedProps} />
+						<Button {...forwardedProps} onClick={this.handleClick} />
 					</Item>
 				)}
 			</DemonButton>
