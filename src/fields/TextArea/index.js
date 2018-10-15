86ca38824db912c3454bff87eac9dd02167ccaab
3went2 <webb.png@gmail.com>
2018-10-15 15:23:41 +0800

    update TextArea.renderQuery()

--- src/fields/TextArea/index.js
+++ src/fields/TextArea/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { TextArea } from 'components/Form';
+import { TextArea, Input } from 'components/Form';
 import Text from 'fields/Text';
 
 export default function TextAreaField(props) {
 	return <Text {...props} component={TextArea} />;
 }
+
+TextAreaField.renderQuery = function renderTable(props) {
+	return <Text {...props} component={Input} />;
+};
