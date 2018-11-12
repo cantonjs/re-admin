d89ecdfa6660f7fcac639bdb19c81d0fe2c2c1eb
3went2 <webb.png@gmail.com>
2018-11-12 17:27:12 +0800

    remove deprecated ModalStore props

--- src/hocs/withModalStore/withModalStoreProvider.js
+++ src/hocs/withModalStore/withModalStoreProvider.js
@@ -1,29 +1,22 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import hoist, { extractRef } from 'hocs/hoist';
-import routerStore from 'stores/routerStore';
 import ModalStore from 'stores/ModalStore';
 import ModalStoreContext from './ModalStoreContext';
-import withModalStore from './withModalStore';
 
 export default function withModalStoreProvider() {
 	return function createModalStoreProviderComponent(WrappedComponent) {
 		@hoist(WrappedComponent)
-		@withModalStore({ prop: 'parentModalStore' })
 		class WithModalStoreProvider extends Component {
 			static propTypes = {
-				parentModalStore: PropTypes.object,
 				modalStore: PropTypes.object,
 			};
 
 			constructor(props) {
 				super(props);
 
-				const { parentModalStore, modalStore } = props;
-				this.modalStore = new ModalStore(
-					modalStore,
-					!parentModalStore && routerStore
-				);
+				const { modalStore } = props;
+				this.modalStore = new ModalStore(modalStore);
 			}
 
 			componentWillUnmount() {
@@ -32,7 +25,7 @@ export default function withModalStoreProvider() {
 
 			render() {
 				const { modalStore, props } = this;
-				const { modalStore: _, parentModalStore, ...other } = props;
+				const { modalStore: _, ...other } = props;
 				return (
 					<ModalStoreContext.Provider value={modalStore}>
 						<WrappedComponent {...extractRef(other)} modalStore={modalStore} />
--- src/stores/ModalStore.js
+++ src/stores/ModalStore.js
@@ -1,19 +1,9 @@
 import { computed, observable, action, observe, keys } from 'mobx';
-import { omitBy, reduce } from 'lodash';
 import Emitter from 'emit-lite';
 
 export default class ModalStore extends Emitter {
-	static prefix = '__';
-
-	// TODO: deprecated
-	static getOmitPaths(val, key) {
-		return key.startsWith(ModalStore.prefix);
-	}
-
 	@observable state = {};
 	@observable modalProps = {};
-
-	_prefix = '__';
 
 	@computed
 	get name() {
@@ -25,15 +15,10 @@ export default class ModalStore extends Emitter {
 		return !!this.name;
 	}
 
-	constructor(parent, router) {
+	constructor(parent) {
 		super();
 
 		this.parent = parent;
-		this._router = router;
-
-		// TODO: deprecated
-		this._bindRouter();
-
 		this._observeDisposer = observe(this, ({ name, newValue }) => {
 			if (name === 'state' && !keys(newValue).length) {
 				this.emit('close');
@@ -44,91 +29,21 @@ export default class ModalStore extends Emitter {
 	@action
 	destroy() {
 		this._observeDisposer();
-		this._historyDisposer && this._historyDisposer();
 		this.state = {};
 	}
 
 	@action
 	close() {
-		if (this._router) this._clearLocation();
 		this.state = {};
 	}
 
 	@action
-	open(state, options = {}) {
-		if (options.router && this._router) this._setLocation(state);
-		else this.state = state;
+	open(state) {
+		this.state = state;
 	}
 
 	@action
 	setModalProps(props) {
-		const ensureNumber = function ensureNumber(propName) {
-			const val = props[propName];
-			if (val && /^\d+$/.test(val)) {
-				props[propName] = +val;
-			}
-		};
-		const ensureBoolean = function ensureBoolean(propName) {
-			if (!props.hasOwnProperty(propName)) {
-				return;
-			}
-			const val = props[propName];
-			if (val === 'false' || val === 'no') {
-				props[propName] = false;
-			} else if (val) {
-				props[propName] = true;
-			}
-		};
-		ensureNumber('width');
-		ensureNumber('zIndex');
-		ensureBoolean('closable');
-		ensureBoolean('confirmLoading');
-		ensureBoolean('mask');
-		ensureBoolean('maskClosable');
 		this.modalProps = props;
-	}
-
-	_bindRouter() {
-		const router = this._router;
-		if (!router) return;
-
-		const handleQueryChange = ({ query }) => {
-			const prefixLength = ModalStore.prefix.length;
-			this.state = reduce(
-				query,
-				(state, val, key) => {
-					if (key.startsWith(ModalStore.prefix)) {
-						const stateKey = key.substr(prefixLength);
-						state[stateKey] = val;
-					}
-					return state;
-				},
-				{}
-			);
-		};
-
-		this._historyDisposer = router.history.listen(handleQueryChange);
-
-		handleQueryChange(router.location);
-
-		this._setLocation = (state) => {
-			const modalQuery = reduce(
-				state,
-				(query, val, key) => {
-					query[`${ModalStore.prefix}${key}`] = val;
-					return query;
-				},
-				{}
-			);
-			const { location } = router;
-			location.query = { ...location.query, ...modalQuery };
-		};
-
-		this._clearLocation = () => {
-			const { location } = router;
-			location.query = omitBy(location.query, (val, key) =>
-				key.startsWith(ModalStore.prefix)
-			);
-		};
 	}
 }
